2a49d23c335d83dd6acf93538f26e7f271192051
para7 <minstrel.suppin@gmail.com>
2018-10-16 21:01:33 +0900

    make output template

--- chieri/main.cpp
+++ chieri/main.cpp
@@ -1,8 +1,28 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using std::cout;
+
+int main(int argc, const char * argv[])
+{
+    
+    bool isJapanese = false;
+    
+    char space = '\n';
+    
+    if(isJapanese)
+    {
+        
+    }
+    {
+        cout << "Height 153cm" << space;
+        cout << "Weight 42kg" << space;
+        cout << "BMI 17.94" << space;
+        cout << "B79-W57-H80" << space;
+        cout << "Birthday 6/11" << space;
+        cout << "Zodiac Gemini" << space;
+        cout << "Blood type A" << space;
+        cout << "Age 16" << space;
+        cout << "Hobby collect four-leaf clover" << space;
+    }
 }
